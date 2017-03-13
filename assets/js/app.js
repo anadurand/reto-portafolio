2b28cf899be7d818be6be2141f44edb70343ba6e
Miriam Peralta <miiriampc@gmail.com>
2017-03-13 18:12:03 -0500

    agregando recursos con texto y boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,24 @@
-function Portafolio(){
-  this.resource=[];
-  this.addResource=function(){
-    recursos.push();
-  }
-  this.removeResource=function(){
+window.addEventListener('load',function(){
+  function Portafolio(){
+    this.resource=[];
+    this.addResource=function(element){
+      resource.push(element);
+    }
+    this.removeResource=function(){
 
+    }
   }
-}
-if (typeof exports != undefined) {
-  exports.Portafolio=Portafolio;
-}
+
+  document.getElementById('r1').onclick=function(){
+    var recurso=prompt("Ingrese un recurso");
+    var button=document.createElement('button');
+    button.innerHTML="X";
+    var elementos=document.getElementById('elementos');
+    elementos.innerHTML= recurso;
+    elementos.appendChild(button);
+  }
+});
+
+// if (typeof exports != undefined) {
+//   exports.Portafolio=Portafolio;
+// }
