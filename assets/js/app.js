7139de2af6ea40deeb381f09805a1c9478ce2afd
Miriam Peralta <miiriampc@gmail.com>
2017-03-14 16:22:53 -0500

    crea recursos con js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,36 @@ window.addEventListener('load',function(){
   function Portafolio(){
     this.resource=[];
     this.addResource=function(element){
-      resource.push(element);
+      this.resource.push(element);
     }
-    this.removeResource=function(){
-
+    this.removeResource=function(ePadre,eHijo){
+      //element.parentNode.removeChild(element);
+      ePadre.removeChild(eHijo);
     }
   }
+  var rec=new Portafolio();
+  var elementos=document.getElementById('elementos');
+  var idSpan=0,idBtn=0, arr=[];
 
-  document.getElementById('r1').onclick=function(){
-    var recurso=prompt("Ingrese un recurso");
-    var button=document.createElement('button');
-    button.innerHTML="X";
-    var elementos=document.getElementById('elementos');
-    elementos.innerHTML= recurso;
-    elementos.appendChild(button);
+  function creaRecursos(){
+    var texto=prompt("Ingrese un recurso");
+    var arrRecurso=texto.split(",");
+    arrRecurso.forEach(function(e){
+        var span=document.createElement('span');
+        span.setAttribute("id",idSpan);
+        span.innerHTML= "  "+e;
+        var btnElimina=document.createElement('button');
+        btnElimina.innerHTML="X";
+        span.appendChild(btnElimina);
+        elementos.appendChild(span);
+        idSpan++;
+        btnElimina.onclick=function(e){
+            elementos.removeChild(span);
+        }
+    });
+  }
+  document.getElementById('r1').onclick=function(e){
+    creaRecursos();
   }
 });
 
