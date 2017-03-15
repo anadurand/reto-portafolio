4f7100eca40e56066208bb38981ac42a5c742efb
miriampc <miiriampc@gmail.com>
2017-03-15 00:08:41 -0500

    funcion creaArticles completa

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,12 @@ window.addEventListener('load',function(){
     this.addResource=function(element){
       this.resource.push(element);
     }
-    this.removeResource=function(ePadre,eHijo){
-      this.resource.splice(this.resource.indexOf(ePadre),1);
-      //ePadre.removeChild(eHijo);
+    this.removeResource=function(idPadre){
+      this.resource.splice(idPadre,1);
     }
   }
   var rec=new Portafolio();
-  var elementos=document.getElementById('elementos');
+  //var elementos=document.getElementById('elementos');
   var section=document.getElementById('section');
   var idSpan=0,idBtn=0, arr=[];
 
@@ -22,21 +21,36 @@ window.addEventListener('load',function(){
     article.setAttribute("class",'idle');
     var divImage=document.createElement('div');
     var divTexto=document.createElement('div');
-    var p=document.createElement('p');
-    var b=document.createElement('b');
-    b.innerHTML="bjstdmngbgr02.thoughtworks.com | "+tipo+ " | "+ip+" | "+direc;
-    p.appendChild(b);
-    divTexto.appendChild(p);
+    var p1=document.createElement('p');
+    var b1=document.createElement('b');
+    var p2=document.createElement('p');
+    var b2=document.createElement('b');
+    var a=document.createElement('a');
+    var span=document.createElement('span');
+    span.setAttribute("id",'elementos');
+    a.setAttribute("href",'#');
+    a.setAttribute("id",'r1');
+    b1.innerHTML="bjstdmngbgr02.thoughtworks.com | "+tipo+ " | "+ip+" | "+direc;
+    p1.appendChild(b1);
+    b2.innerHTML="+ ";
+    p2.appendChild(b2);
+    a.innerHTML="Specify Resources";
+    p2.appendChild(a);
+    span.innerHTML=" | Resources: ";
+    p2.appendChild(span);
+    divTexto.appendChild(p1);
+    divTexto.appendChild(p2);
     article.appendChild(divTexto);
     section.appendChild(article);
   }
 creaArticles();
+
   function creaRecursos(){
     var texto=prompt("Ingrese un recurso");
     var arrRecurso=texto.split(",");
     arrRecurso.forEach(function(e){
         var span=document.createElement('span');
-        span.setAttribute("id","sp"+idSpan);
+        span.setAttribute("id",idSpan);
         span.innerHTML= "  "+e;
         var btnElimina=document.createElement('button');
         btnElimina.innerHTML="X";
@@ -44,15 +58,16 @@ creaArticles();
         elementos.appendChild(span);
         idSpan++;
         btnElimina.onclick=function(e){
+          var idPadre=e.target.parentNode.getAttribute('id');
             elementos.removeChild(span);
-            alert(rec.resource);
+            rec.removeResource(idPadre);
+            //document.body.appendChild(elementos);
         }
-        //lert(rec.resource);
         rec.addResource(elementos);
     });
+
   }
   document.getElementById('r1').onclick=function(){
-    //alert("aki");
     creaRecursos();
   }
 });
