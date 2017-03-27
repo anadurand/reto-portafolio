106557f8c665b78ba3e1a88932e233d4738242d8
strongwarmi <elianasc10@hotmail.com>
2017-03-27 09:56:33 -0500

    pestaña de tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,4 @@
-function Portafolio(){
-  this.resource=[];
-  this.addResource=function(){
-    recursos.push();
-  }
-  this.removeResource=function(){
-
-  }
-}
-if (typeof exports != undefined) {
-  exports.Portafolio=Portafolio;
-}
+var agents = [ { nombre : "bjstdmngbgr02.thoughtworks.com", estado : "idle",  ip : "192.168.1.2", directorio : "/var/lib/cruise-agent" },
+			 { nombre : "bjstdmngbgr02.thoughtworks.com", estado : "idle",  ip : "192.168.1.2", directorio : "/var/lib/cruise-agent"  },
+			 {nombre : "bjstdmngbgr02.thoughtworks.com", estado : "idle",  ip : "192.168.1.2", directorio : "/var/lib/cruise-agent" },
+			 {nombre : "bjstdmngbgr02.thoughtworks.com", estado : "idle",  ip : "192.168.1.2", directorio : "/var/lib/cruise-agent" }];
